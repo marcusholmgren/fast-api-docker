d44faee9bc58d225f4e59d45f913a02f28ba4955
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-03-06 11:51:53 +0100

    fix: unit test formatting

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -6,4 +6,6 @@ def test_ping(test_app):
 
     # Then
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
+    assert response.json() == {
+        "environment": "dev",
+        "ping": "pong", "testing": True}
