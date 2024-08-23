45c424b1aa8edab6ff3ff9fc576a2e243be4ff18
Marcus Holmgren <marcus.holmgren1@gmail.com>
2024-08-23 10:52:10 +0200

    App structure (#5)
    * App Structure

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,4 +1,5 @@
 from tortoise import fields, models
+from pydantic import BaseModel
 
 
 class TextSummary(models.Model):
@@ -8,3 +9,14 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+
+class SummaryPayloadSchema(BaseModel):
+    url: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "url": "https://example.com"
+            }
+        }
