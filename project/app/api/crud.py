a8f3ac2f824201f12a26030b8c588c00e6573c7c
Marcus Holmgren <marcus.holmgren1@gmail.com>
2024-08-23 16:02:44 +0200

    RESTful Routes (#6)
    Add the summary route with tests

---
+++ project/app/api/crud.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Union
+
+from fastapi import HTTPException
+from tortoise.exceptions import IntegrityError
+
+from app.models.pydantic import SummaryPayloadSchema
+from app.models.tortoise import TextSummary
+
+log = logging.getLogger(__name__)
+
+
+async def post(payload: SummaryPayloadSchema) -> int:
+    summary = TextSummary(url=payload.url, summary="dummy dummy")
+    try:
+        await summary.save()
+    except IntegrityError as e:
+        log.error(f"IntegrityError: {e}")
+        raise HTTPException(
+            status_code=409, detail=f"URL '{payload.url}' already exists"
+        )
+    return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary
+    return None
